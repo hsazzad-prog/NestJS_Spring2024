0480a3a7f63034acd6cb89885d50c4a5dd05b4a2
hsazzad-prog <hsazzad02@gmail.com>
2024-02-23 18:09:33 +0600

    file Upload

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Post, Query, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query, Res, UploadedFile, UseInterceptors, UsePipes, ValidationPipe } from "@nestjs/common";
 import { AdminService } from "./admin.service";
 import { AdminDTO } from "./admin.dto";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { MulterError, diskStorage } from "multer";
 
 
 
@@ -21,10 +23,34 @@ export class AdminController{
     @Query('id') id:string) : object{
         return this.adminService.getUsersByNameAndId(name, id);
     }
+
     @Post('adduser')
+    @UseInterceptors(FileInterceptor('myfile',
+{ fileFilter: (req, file, cb) => {
+  if (file.originalname.match(/^.*\.(jpg|webp|png|jpeg)$/))
+   cb(null, true);
+  else {
+   cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
+   }
+  },
+  limits: { fileSize: 30000 },
+  storage:diskStorage({
+  destination: './upload',
+  filename: function (req, file, cb) {
+   cb(null,Date.now()+file.originalname)
+  },
+  })
+}
+))
     @UsePipes(new ValidationPipe)
-    async addUser(@Body() myobj: AdminDTO): Promise<AdminDTO>{
+    async addUser(@Body() myobj: AdminDTO,  @UploadedFile()  myfile: Express.Multer.File): Promise<AdminDTO>{
         console.log(myobj.name);
         return this.adminService.addUser(myobj);
     }
+
+    @Get('/getimage/:name')
+    getImages(@Param('name') name:string, @Res() res) {
+    res.sendFile(name,{ root: './upload' })
+    }
+   
 }
